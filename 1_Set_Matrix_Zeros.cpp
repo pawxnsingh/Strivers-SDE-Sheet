ddd06fddd0bc5b25e8f0fa03cf81b2e530d335fd
Pawan Singh Dogra <48354037+pawxnsingh@users.noreply.github.com>
2023-06-16 16:02:36 +0530

    Update 1_Set_Matrix_Zeros.cpp

--- 1_Set_Matrix_Zeros.cpp
+++ 1_Set_Matrix_Zeros.cpp
@@ -81,7 +81,57 @@ void setZeros(vector<vector<int>> &matrix){
 
 
 //Optimal Solution - 
+//Time Complexity --- > O(N * M)
+//Space Complexity ----> O(1)
 
+void setZeros(vector<vector<int>> &matrix){
+	int rSize = matrix.size();
+	int cSize = matrix[0].size();
+	
+	//row -- > matrix [..][0]
+	//col -- > matrix [0][..]
+	int col0 = 1;
+	// marking the inplace checker 
+	for(int i=0;i<rSize;i++){
+		for(int j=0;j<cSize;j++){
+			if(matrix[i][j] == 0){
+				matrix[i][0] = 0;
+				if(j!=0){ // if j==0 you cant Mark it Here 
+				// instead you mark in col0
+					matrix[0][j] = 0;
+				}
+				else{
+					col0 = 0;
+				}
+			}
+		}
+	}
+
+	// marking Zero Inner Matrix 
+	for(int i=1;i<rSize;i++){
+		for(int j=1;j<cSize;j++){
+			if(matrix[i][j] != 0){
+				if(matrix[i][0] == 0 || matrix[0][j] == 0){
+					matrix[i][j] = 0;
+				}
+			}
+		}
+	}
+	
+	// marking Checker equal To zero 
+
+	if(matrix[0][0] == 0){
+		for(int j=0;j<cSize;j++){
+			matrix[0][j] = 0;
+		}
+	}
+
+	if(col0 == 0){
+		for(int i=0;i<rSize;i++){
+			matrix[i][0] = 0 ;
+		}
+	}
+}
 
 
 
