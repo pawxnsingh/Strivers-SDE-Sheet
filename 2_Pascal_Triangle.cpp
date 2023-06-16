3bf549afc06e14c6a5f7cfc5b5f451654eb7f0ba
Pawan Singh Dogra <48354037+pawxnsingh@users.noreply.github.com>
2023-06-16 17:07:25 +0530

    Create 2_Pascal_Triangle.cpp

---
+++ 2_Pascal_Triangle.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+    In the Pascal Triangle There Are Usually Three Type of Problem that is Generally Asked
+    1. Print the Element by Giving Row and Column 
+    2. Print Nth Row of The Pascal Triangle
+    3. Print The Entire Pascal Triangle by just The Number of Row As Input
+*/
+
+//1. Print the Element by Giving Row and Column 
+
+
+
