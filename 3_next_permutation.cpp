29f579ee137dbcb0234f7c701f10db4577ca679e
Pawan Singh Dogra <48354037+pawxnsingh@users.noreply.github.com>
2023-06-17 15:58:58 +0530

    Update 3_next_permutation.cpp

--- 3_next_permutation.cpp
+++ 3_next_permutation.cpp
@@ -27,6 +27,75 @@ using namespace std;
 
 // Optimal Approach: -
 
+                        
+//                Time Complexity ---  >  O(N) + O(N) + O(N) 
+//                                        O(3N)
+               
+//                Space Complexity  ---- > if Interviewer Allow Us to Modify The array O(1)
+//                                                or we Can Take O(N)
+                                        
+
+
+
+
+
+
+
+
+
+
+
+            #include <bits/stdc++.h> 
+            void rev(vector<int> &permutation , int index , int n){
+                int i = index;
+                int j = n;
+                while(i<j){
+                    swap(permutation[i],permutation[j]);
+                    i++;
+                    j--;
+                } 
+            }
+
+            vector<int> nextPermutation(vector<int> &permutation, int n){
+                //Find The Break Point That has Been found
+                int index = -1;
+                for(int i = n-2 ;i > 0 ;i--){
+                    if(permutation[i] < permutation[i+1]){
+                        index = i ;
+                        break;
+                    }
+                }
+
+                if(index == -1 ){
+                    reverse(permutation.begin(),permutation.end());
+                    return permutation;
+                }
+
+                // find small but greater than index == i
+
+                int smallmaxindex = -1;
+                for(int i = n-1 ; i>index ; i--){
+                    if(permutation[i] > permutation[index]){
+                        swap(permutation[i],permutation[index]);
+                        break;
+                    }
+                }
+
+
+                // now Reverse the index+1 to get the next permutation which is just greater
+                // than the given vector
+
+                rev(permutation,index,n);
+                        
+                reverse(permutation.begin() + index + 1 , )
+                        
+                return permutation;
+
+
+
+
+    
+}
 
     
   
