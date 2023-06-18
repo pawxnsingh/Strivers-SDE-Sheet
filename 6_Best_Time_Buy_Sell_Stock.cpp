51c77a5391d8b4263c630339ce95309dfa640fce
Pawan Singh Dogra <48354037+pawxnsingh@users.noreply.github.com>
2023-06-18 13:15:45 +0530

    Update 6_Best_Time_Buy_Sell_Stock.cpp

--- 6_Best_Time_Buy_Sell_Stock.cpp
+++ 6_Best_Time_Buy_Sell_Stock.cpp
@@ -1,1 +1,51 @@
+Brute ---------------->>>
 
+Time ----------> O(N^2)
+Space ---------> O(1)
+  
+      #include <bits/stdc++.h> 
+      int maximumProfit(vector<int> &prices){
+          int size = prices.size();
+          int profit = 0;
+
+          for(int i = 0; i < size ;i++){
+              int buy = prices[i];
+              for(int j = i+1;j<size;j++){
+                  int sell = prices[j] - buy ; 
+                  // if(sell > profit){
+                  //     profit = sell;
+                  // }
+                  profit = max(sell,profit);
+              }
+          }
+          return profit;
+      }
+
+
+Better/Optimal ---------------->>>> 
+Time -----------> O(N)
+Space --------->>> O(1)
+              
+      #include <bits/stdc++.h> 
+      int maximumProfit(vector<int> &prices){
+          int size = prices.size();
+
+          int bestBuy = prices[0];
+          int max_Profit = 0;
+
+          for(int i=1;i<size;i++){
+
+              if(prices[i] < bestBuy){
+                  bestBuy = prices[i];
+              }
+
+              else if(prices[i] - bestBuy > max_Profit){
+                  max_Profit = prices[i] - bestBuy;
+              }   
+          }
+          return max_Profit;   
+      }
+  
+  
+  
+  
