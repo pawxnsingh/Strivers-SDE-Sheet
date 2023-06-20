03b7f60b8b9dab823899e90dbf318d3205d60f59
Pawan Singh Dogra <48354037+pawxnsingh@users.noreply.github.com>
2023-06-20 16:38:56 +0530

    Update 9_mergeTwoSortedArray.cpp

--- 9_mergeTwoSortedArray.cpp
+++ 9_mergeTwoSortedArray.cpp
@@ -1,6 +1,6 @@
 WITH USING EXTRA SPACE ------------------->
-  TIME --------------->>> O(N)
-  SPACE --------------->>>> O(N)
+  TIME --------------->>> O(N+M)
+  SPACE --------------->>>> O(N+M)
 
         vector<int> ninjaAndSortedArrays(vector<int>& arr1, vector<int>& arr2, int m, int n) {
           vector<int>ans;
@@ -35,9 +35,68 @@ WITH USING EXTRA SPACE ------------------->
 
 
 WIHTOUT USING EXTRA SPACE-------------------------------->>>>>>
-  TIME ------------------------->>> O(N)
+  TIME ------------------------->>> O(MIN(M+N)) + O(NLOGN) + O(MLOGM)
   SPACE --------------------->>>>>>> O(1)
-  
+	
+
+		void merge(long long arr1[], long long arr2[], int n, int m){
+		int i = n-1 ; 
+		int j = 0;
+
+		while(i >= 0  && j < m){
+			if(arr1[i] >= arr2[j]){
+				swap(arr1[i],arr2[j]);
+				i--;
+				j++;
+			}
+
+			else{
+				break;
+			}
+		}
+
+		sort(arr1,arr1+n);
+
+		sort(arr2,arr2+m);
+
+	} 
+
+
+Another Optimal Solution -------------->
+	Time ------------------->>> O(nLogn)
+	Space ----------------->>>>> O(1)
+			
+        void swapIfGreater(long long arr1[], long long arr2[],int ind1 , int ind2){
+            if(arr1[ind1]>arr2[ind2]){
+                swap(arr1[ind1],arr2[ind2]);
+            }
+        }
+
+		void merge(long long arr1[], long long arr2[], int n, int m) { 
+		int len = (n+m);
+		int gap = (len/2) + (len%2);
+		while(gap > 0){
+			int left = 0;
+			int right = left + gap;
+			while(right < len){
+				if(left < n && right >= n ){
+					swapIfGreater(arr1,arr2,left,right-n);
+				}
+				else if(left >= n){
+					swapIfGreater(arr2,arr2,left-n,right-n);
+				}
+				else{
+					swapIfGreater(arr1,arr1,left,right);
+				}
+				left++;
+				right++;
+			}
+			if(gap == 1) break;
+			gap = (gap/2) + (gap%2);
+		}
+	} 
+
+	
   
   
   
