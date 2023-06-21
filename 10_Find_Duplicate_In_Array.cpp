564e121abb037e64e33dba6586774260eef124cc
Pawan Singh Dogra <48354037+pawxnsingh@users.noreply.github.com>
2023-06-21 08:58:52 +0530

    Update 10_Find_Duplicate_In_Array.cpp

--- 10_Find_Duplicate_In_Array.cpp
+++ 10_Find_Duplicate_In_Array.cpp
@@ -1,1 +1,59 @@
-B
+BruteForce ------------>>>
+  Time ---------------->> O(N^2)
+  Space ---------------->>> O(1)
+
+
+      #include <bits/stdc++.h>
+      int findDuplicate(vector<int> &arr, int n){
+      	int dup = -1;
+      	for(int i=0;i<n;i++){
+      		for(int j=i+1;j<n;j++){
+      			if(arr[i] == arr[j]){
+      				dup = arr[i];
+      			}
+      		}
+      	}
+      	return dup;
+      }
+
+
+Better -------------->>>
+	Time --------->>>>>>O(Nlogn) + O(N)
+	Space -------------->>>> O(1)
+
+
+
+	int findDuplicate(vector<int> &arr, int n){
+	int dupElement = 0;
+	//Sort The Entire Array
+		sort(arr.begin(),arr.end());
+		for(int i=0;i<n;i++){
+			if(arr[i] == arr[i+1]){
+				dupElement = arr[i];
+			}
+		}
+		return dupElement;
+	}
+
+
+More Better------------------>>>
+	Time ------------->>
+	Space------------->>
+
+	int findDuplicate(vector<int> &arr, int n){
+		unordered_map<int,int>temp;
+		int dupEle=0;
+		for(int i=0;i<n;i++){
+			temp[arr[i]]++;
+		}
+		for(auto it:temp){
+			if(it.second >= 2){
+				dupEle = it.first;		
+			}
+		}
+		return dupEle;
+	}
+
+
+
+
