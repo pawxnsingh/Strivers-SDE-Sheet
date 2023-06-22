869147fa4272431e8629b688205f8feda6cce13b
Pawan Singh Dogra <48354037+pawxnsingh@users.noreply.github.com>
2023-06-22 17:34:45 +0530

    Update 16_Majority_Element(>N3).cpp

--- 16_Majority_Element(>N3).cpp
+++ 16_Majority_Element(>N3).cpp
@@ -1,6 +1,6 @@
 Brute---------------->>>
-    Time ------------------>>> O()
-    Space  --------------->>> O()
+    Time ------------------>>> O(N^2)
+    Space  --------------->>> O(1) We are Using Extra Space only to Store  Two Element which is as good as Constant
 
 Question :- How Many Integer At Max be Their in Answer??
     Ans :- for let take an example of size N = 8;
@@ -12,28 +12,40 @@ Question :- How Many Integer At Max be Their in Answer??
             More Than N/3
 
 
-#include <bits/stdc++.h>
-
-vector<int> majorityElementII(vector<int> &arr){
-    vector<int>ans;
-    int size = arr.size();
-
-    for(int i=0;i<size;i++){
-        if(ans.empty() || ans.back() != arr[i]){
-            int count = 0;
-            for(int j=0;j<size;j++){
-                if(arr[j] == arr[i]){
-                    count++;
+    #include <bits/stdc++.h>
+    
+    vector<int> majorityElementII(vector<int> &arr){
+        vector<int>ans;
+        int size = arr.size();
+    
+        for(int i=0;i<size;i++){
+            if(ans.empty() || ans.back() != arr[i]){
+                int count = 0;
+                for(int j=0;j<size;j++){
+                    if(arr[j] == arr[i]){
+                        count++;
+                    }
                 }
-            }
-            if(count > size/3){
-                ans.push_back(arr[i]);
-                if(ans.size() == 2){
-                    break;
+                if(count > size/3){
+                    ans.push_back(arr[i]);
+                    if(ans.size() == 2){
+                        break;
+                    }
                 }
             }
         }
+        return ans;
     }
-    return ans;
-}
+
+
+Better--------------------->>>
+    Time -------------------------->>O()
+    Space----------------------------->>> O()
+
+
+
+
+
+
+
 
