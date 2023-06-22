f91c382f8a450c05cadc5c4b6b7a4ec8dacc3402
Pawan Singh Dogra <48354037+pawxnsingh@users.noreply.github.com>
2023-06-22 17:15:39 +0530

    Update and rename 16_Majority_Element(N3).cpp to 16_Majority_Element(>N3).cpp

---
+++ 16_Majority_Element(>N3).cpp
@@ -0,0 +1,39 @@
+Brute---------------->>>
+    Time ------------------>>> O()
+    Space  --------------->>> O()
+
+Question :- How Many Integer At Max be Their in Answer??
+    Ans :- for let take an example of size N = 8;
+            if we divide >N/3 = 2
+                at minimum the integer has to appear 3 Time
+                if 3 + 3 + 3 = 9 integer
+                where n is just 8 integer 
+            So There Will be at Max 2 Integer as answer That will be 
+            More Than N/3
+
+
+#include <bits/stdc++.h>
+
+vector<int> majorityElementII(vector<int> &arr){
+    vector<int>ans;
+    int size = arr.size();
+
+    for(int i=0;i<size;i++){
+        if(ans.empty() || ans.back() != arr[i]){
+            int count = 0;
+            for(int j=0;j<size;j++){
+                if(arr[j] == arr[i]){
+                    count++;
+                }
+            }
+            if(count > size/3){
+                ans.push_back(arr[i]);
+                if(ans.size() == 2){
+                    break;
+                }
+            }
+        }
+    }
+    return ans;
+}
+
