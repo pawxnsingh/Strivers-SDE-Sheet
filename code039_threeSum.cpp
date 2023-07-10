95a0e4a582751e7d7782b24590f3574d68e5235a
Pawan Singh Dogra <48354037+pawxnsingh@users.noreply.github.com>
2023-07-10 15:37:57 +0530

    Update code039_threeSum.cpp

--- code039_threeSum.cpp
+++ code039_threeSum.cpp
@@ -53,3 +53,44 @@ better ---------------->>>>
         	vector<vector<int>>ans(st.begin(),st.end());
         	return ans;
         }
+
+
+
+Optimal --------------->>>
+    Time ----------->>> O(Nlogn) + O(N*N)
+    Space ----------->>>> O(3k)
+    
+        vector<vector<int>> findTriplets(vector<int>arr, int n, int K) {
+        	sort(arr.begin(),arr.end());
+        	vector<vector<int>>ans;
+        	for(int i=0;i<n;i++)
+        	{
+        		if(i>0 && arr[i] == arr[i-1]) continue;	
+        		int j=i+1;
+        		int k=n-1;
+        		while(j<k)
+        		{
+        			long long sum = arr[i];
+        			sum += arr[j];
+        			sum += arr[k];
+        			if(sum < K)
+        			{
+        				j++;
+        			}
+        			else if(sum > K)
+        			{
+        				k--;
+        			}
+        			else
+        			{
+        				vector<int>v1 = {arr[i],arr[j],arr[k]};
+        				ans.push_back(v1);
+        				j++;
+        				k--;
+        				while(j<k && arr[j] == arr[j-1]) j++;
+        				while(j<k && arr[k] == arr[k+1]) k--;
+        			}
+        		}
+        	}
+        	return ans;
+        }
