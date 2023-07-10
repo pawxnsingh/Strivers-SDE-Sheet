dff3b5353c7ec774dd18b3b2537f05cfd7ec72b4
Pawan Singh Dogra <48354037+pawxnsingh@users.noreply.github.com>
2023-07-10 07:28:51 +0530

    Update 38_copy_linked_list_with_random_pointer.cpp

--- 38_copy_linked_list_with_random_pointer.cpp
+++ 38_copy_linked_list_with_random_pointer.cpp
@@ -1,1 +1,64 @@
+better ----------->>>>
+    Time -------->>> O(N)
+    Space ---------->> O(N) using map data Structure
+
+
+        LinkedListNode<int> *insertAtTail(LinkedListNode<int>* &cloneHead,LinkedListNode<int>* &cloneTail, int data)
+        {
+            LinkedListNode<int> *newNode = new LinkedListNode<int>(data);
+            if(cloneHead == NULL && cloneTail == NULL)
+            {
+                cloneHead = newNode;
+                cloneTail = newNode;
+            }
+            else
+            {
+                cloneTail -> next = newNode;
+                cloneTail = newNode;
+            }
+        }
+        LinkedListNode<int> *cloneRandomList(LinkedListNode<int> *head)
+        {
+            LinkedListNode<int> *cloneHead = NULL;
+            LinkedListNode<int> *cloneTail = NULL;
+            //Deep Copy of linked list using next pointer]
+            LinkedListNode<int> *temp = head;
+            while(temp != NULL)
+            {
+                insertAtTail(cloneHead,cloneTail,temp -> data);
+                temp = temp -> next;
+            }
+            //Mapping of the element in both the linked list 
+            //in a map data stucture 
+            map<LinkedListNode<int>*,LinkedListNode<int>*>Mapping;
+            LinkedListNode<int> *originalNode = head;
+            LinkedListNode<int> *cloneNode = cloneHead;
+            while(originalNode && cloneNode)
+            {
+                Mapping[originalNode] = cloneNode;
+                originalNode = originalNode -> next;
+                cloneNode = cloneNode -> next;
+            }
+            originalNode = head;
+            cloneNode = cloneHead;
+            while(originalNode && cloneNode)
+            {
+                cloneNode -> random = Mapping[originalNode -> random];
+                originalNode = originalNode -> next;
+                cloneNode = cloneNode -> next;
+            }
+            return cloneHead;
+        }
+
+
+
+optimised ------------>>>
+    Time --------->>> O(N)
+    Space --------->>>> O(1)
+
+
+
+    
+
+
 
